c7b099664ffd88c4ab2f0b90d74a8e95c3497f0b
Your Name <you@example.com>
2025-11-13 13:31:59 +0100

    Cambio de prueba en examen_daw.php

--- examen_daw.php
+++ examen_daw.php
@@ -7,6 +7,7 @@
 </head>
 <body>
     <?php
+    // Cambio de prueba para confirmar subida a GitHub
     // Creo las variables
     $nombre_alumno = "Jiaxin Ji";
     $modulo = "Desarrollo Web en Entorno Servidor";
